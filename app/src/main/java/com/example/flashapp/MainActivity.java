5e6e9ed8376bbacbaee9e4f309e4d7220e259540
bpachhai <bpachhai@uno.edu>
2021-02-19 18:07:09 -0600

    first

---
+++ app/src/main/java/com/example/flashapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.flashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+   private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.buttonAnother);
+
+
+    }
+}
