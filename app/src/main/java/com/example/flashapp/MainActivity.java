f368b0a606c59c748487dd1bc33b0eaa53cd79e9
bpachhai <bpachhai@uno.edu>
2021-02-24 16:34:21 -0600

    V1.0 of lab1 uploaded

--- app/src/main/java/com/example/flashapp/MainActivity.java
+++ app/src/main/java/com/example/flashapp/MainActivity.java
@@ -3,18 +3,41 @@ package com.example.flashapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-   private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         final TextView answer = (TextView) findViewById(R.id.answer);
+        answer.setVisibility(View.INVISIBLE);
+        final TextView question = (TextView) findViewById(R.id.question);
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        button = (Button) findViewById(R.id.buttonAnother);
+                answer.setVisibility(View.VISIBLE);
+                question.setVisibility(View.INVISIBLE);
+            }
+        });
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                answer.setVisibility(View.INVISIBLE);
+                question.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
+
+
 
 
     }
